12ba8e2451fd8811998d1532df26c016479f1738
Ivan Privalov <Ivanprivalov28@gmail.com>
2021-12-15 02:12:21 +0300

    full dz

---
+++ dz_1.js
@@ -0,0 +1,75 @@
+//-#1 
+
+    let str = "Привет";
+    let num = 123;
+    let flag = true;
+    let txt = "true"
+
+console.log(typeof str);
+console.log(typeof num);
+console.log(typeof flag);
+console.log(typeof txt);
+
+//-#2
+
+    let i = 5;
+    let k = 3;
+
+        let sum = i+k;
+        let sub = i-k;
+        let mul = i*k;
+        let div = i/k;
+
+console.log(sum);
+console.log(sub);
+console.log(mul);
+console.log(div);
+
+//-#3
+
+    let ka = "3";
+    let iv = "5";
+    let a = 75;
+    let b = "CG";
+
+        let a1 = i%k;
+        let a2 = k%i;
+        let a3 = i+ka;
+        let a4 = iv-k;
+        let a5 = a+b;
+
+console.log(a1);
+console.log(a2);
+console.log(a3);
+console.log(a4);
+console.log(a5);
+
+//-#4
+
+    let height = 23;
+    let width = 10;
+
+        let s = (height*width/2);
+
+console.log(s);
+
+//-#5
+
+    let heightC = 10;
+    let dC = 4;
+
+        let v = Math.PI*((dC/2)**2)*heightC; 
+
+console.log(v);
+
+//-#5
+
+    let n = 3;
+    let m = 4;
+
+        let c = Math.pow(n,2)+Math.pow(m,2);
+
+console.log(c);
+
+
+
