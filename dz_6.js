fee4c6bbbc76970de2f176c72bab7b7be928ca2e
Ivan Privalov <Ivanprivalov28@gmail.com>
2022-01-17 04:13:28 +0300

    dz6

---
+++ dz_6.js
@@ -0,0 +1,48 @@
+//#1
+// let first = document.querySelector('.first')
+
+// first.addEventListener('click', First => {
+//     alert('Welcome to the club')
+// })
+
+//#2
+
+// let second = document.querySelector('.second')
+
+// second.addEventListener('mouseover', Second => {
+//     alert('You are the best!!!')
+// })
+
+//#3
+
+// let third = document.querySelector('.third')
+
+// third.addEventListener('dblclick', Third => {
+//     alert('The Weekend!')
+// })
+
+//#4
+
+// let fourth = document.querySelector('.fourth')
+
+// fourth.addEventListener('mouseout', Fourth => {
+//     alert('Hi Man')
+// })
+
+//#5
+
+// let elem = document.querySelector('.elem')
+// let dz5 = document.querySelector('.dz5')
+
+// elem.addEventListener('click', Elem => {
+//     alert(dz5.value)
+// })
+
+//#6
+
+// let elem2 = document.querySelector('.elem2')
+// let dz6 = document.querySelector('.dz6')
+
+// elem2.addEventListener('click', Elem2 => {
+//     dz6.value = 'Forget me'
+// })
