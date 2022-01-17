318415e4c43a98a73e6ef2d4778c62fd60d73f5c
Ivan Privalov <Ivanprivalov28@gmail.com>
2022-01-18 02:19:49 +0300

    dz6 update

--- dz_6.js
+++ dz_6.js
@@ -46,3 +46,65 @@
 // elem2.addEventListener('click', Elem2 => {
 //     dz6.value = 'Forget me'
 // })
+
+//#7
+
+// let elem3 = document.querySelector('.elem3')
+// let img = document.querySelector('.img')
+// elem3.addEventListener('click', Elem3 => {
+//     img.src = 'i.jpg'
+// })
+
+//#9 
+
+// let dz9 = document.querySelector('.dz9')
+
+// dz9.addEventListener('click', Dz9 => {
+//     dz9.value = 'Ow sh**, here we go again'
+// })
+
+//#11
+
+// let dz11 = document.querySelector('.dz11')
+
+// function Dz11() {
+//     this.value = 'yeeeeh!!!!!'
+// }
+
+// dz11.addEventListener('click', Dz11)
+
+//#12
+
+// let dz12 = document.querySelector('.dz12')
+
+// function Dz12() {
+//     this.disabled = true
+//     this.value = 'stop push'
+// }
+
+// dz12.addEventListener('click', Dz12)
+
+//#13
+// let img2 = document.querySelector('.img2')
+
+// function Dz13() {
+//     this.src = 'i.jpg'
+// }
+
+// function Dz131() {
+//     this.src = 'i (1).jpg'
+// }
+
+// img2.addEventListener('mouseover', Dz13)
+// img2.addEventListener('mouseout', Dz131)
+
+//#14
+
+// let dz14 = document.querySelector('.dz141')
+// let dz1411 = document.querySelector('.dz1411')
+
+// function Dz14() {
+//     dz1411.classList.add('dz14')
+// }
+
+// dz14.addEventListener('click', Dz14)
